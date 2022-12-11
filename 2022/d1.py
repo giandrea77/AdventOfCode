3c2acb3b2a1e52e155a40f54a1205627e0270d85
Andrea <andrea.girardi@gmail.com>
2022-12-11 11:46:01 +0100

    Advent of code: day1

---
+++ 2022/d1.py
@@ -0,0 +1,19 @@
+#
+# Advent of code: Day1
+#
+
+calories = {0:0}
+index = 0
+
+with open('input.txt') as file_input:
+
+    for line in file_input:
+        
+        if line.isspace():
+            index += 1
+            calories[index] = 0            
+        else:
+            calories[index] = calories[index] +  int(line)        
+    
+max_value = max(calories.values())  # maximum value
+print(max_value)
---
+++ 2022/d2.py
@@ -0,0 +1,21 @@
+#
+# Advent of code: Day1
+#
+
+calories = {0:0}
+index = 0
+
+with open('input.txt') as file_input:
+
+    for line in file_input:
+        
+        if line.isspace():
+            index += 1
+            calories[index] = 0            
+        else:
+            calories[index] = calories[index] +  int(line)
+
+calories_array = list(calories.values())
+calories_array = sorted(calories_array, reverse=True)
+
+print (sum(calories_array[0:3]))
